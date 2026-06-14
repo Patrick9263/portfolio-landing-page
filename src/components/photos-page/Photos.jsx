189cfca2890fec1360e17ec0d68b07103df5b333
Patrick Smith <psmith9263@gmail.com>
2026-06-14 02:10:40 -0400

    Polish layout and photo page styles (#20)

--- src/components/photos-page/Photos.jsx
+++ src/components/photos-page/Photos.jsx
@@ -4,15 +4,17 @@ import './PhotosPage.css'
 const Photos = () => {
   return (
     <Section title="Photos">
-      <a
-        href="/?photos=true"
-        rel="noopener noreferrer"
-        className="open-gallery-anchor"
-      >
-        <button className="open-gallery-button" type="button">
-          Open gallery
-        </button>
-      </a>
+      <div className="photos-preview">
+        <a
+          href="/?photos=true"
+          rel="noopener noreferrer"
+          className="open-gallery-anchor"
+        >
+          <button className="open-gallery-button" type="button">
+            Open gallery
+          </button>
+        </a>
+      </div>
     </Section>
   )
 }
--- src/components/photos-page/PhotosPage.jsx
+++ src/components/photos-page/PhotosPage.jsx
@@ -4,34 +4,43 @@ import PhotoAlbum from 'react-photo-album'
 import photoDimensions from '../../images/downscaled-photos/output.json'
 import './PhotosPage.css'
 
+const photos = photoDimensions.map((photo) => ({
+  ...photo,
+  width: Number(photo.width),
+  height: Number(photo.height),
+}))
+
 export default function PhotosPage() {
   return (
-    <Fade bottom duration={1000} distance="20px">
-      <div className="photos">
-        <Navbar top />
-        <Fade duration={1000}>
-          <div className="about-text">
-            <h2>Photos</h2>
-            <p>Photography is one of my hobbies, enjoy some that I've taken!</p>
-            <p>Currently I'm using a Sony A7R V with a 24mm lens.</p>
-            <p>Click on a photo to see the full resolution.</p>
-          </div>
-        </Fade>
-        <div className="photo-container">
-          <PhotoAlbum
-            layout="rows"
-            photos={photoDimensions}
-            onClick={({ index }) => {
-              window.open(
-                photoDimensions[index].src
-                  .replace('downscaled/', '')
-                  .replace('.jpeg', '.png'),
-                '_blank'
-              )
-            }}
-          />
+    <div className="photos-page">
+      <Navbar top />
+
+      <Fade duration={1000}>
+        <div className="photos-intro">
+          <h2>Photos</h2>
+          <p>Photography is one of my hobbies. Enjoy some that I've taken!</p>
+          <p>Currently I'm using a Sony A7R V with a 24mm lens.</p>
+          <p>Click on a photo to see the full resolution.</p>
         </div>
+      </Fade>
+
+      <div className="photo-container">
+        <PhotoAlbum
+          layout="rows"
+          photos={photos}
+          targetRowHeight={320}
+          spacing={10}
+          onClick={({ index }) => {
+            window.open(
+              photos[index].src
+                .replace('downscaled/', '')
+                .replace('.jpeg', '.png'),
+              '_blank',
+              'noopener,noreferrer'
+            )
+          }}
+        />
       </div>
-    </Fade>
+    </div>
   )
 }
